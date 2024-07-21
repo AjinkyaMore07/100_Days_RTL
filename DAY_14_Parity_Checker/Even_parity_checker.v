bc24f670c706b3faebd0e6de22a848280b4effcd
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2024-07-21 08:18:41 +0530

    Update Even_parity_checker.v

--- DAY_14_Parity_Checker/Even_parity_checker.v
+++ DAY_14_Parity_Checker/Even_parity_checker.v
@@ -1,1 +1,6 @@
 
+module parity_generator(input [7:0]in,input parity_in , output error);
+  wire w1;
+  assign w1 = (in[0] ^ in[1])^(in[2] ^ in[3])^(in[4] ^ in[5])^(in[6] ^ in[7]) ^ parity_in ;  
+  assign error = w1 ? 1'b1 : 1'b0;
+endmodule

1c897bed59775902b8eab295bb27d36e28655f6f
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2024-07-25 12:00:10 +0530

    D_FF_without_asynchronous_reset.v

---
+++ DAY_23_D_FF_without_asynchronous_reset/D_FF_without_asynchronous_reset.v
@@ -0,0 +1,15 @@
+module D_FF_without_asynchronous_reset (
+    input wire d,
+    input wire clk,
+    input wire reset,
+    output wire q
+);
+  reg temp;
+  always @(posedge clk or negedge reset) begin
+    if (!reset)
+      temp <= 0;
+    else
+      temp <= d;
+  end
+  assign q = temp;
+endmodule
